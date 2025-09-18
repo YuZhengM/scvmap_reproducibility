bee53cdb010382fd2bd90a497afc9ed2b8aaf58c
yuzhengmin <3236170161@qq.com>
2025-09-18 20:05:09 +0800

    follow

--- R/RResult/README.md
+++ R/RResult/README.md
@@ -2,3 +2,8 @@
 ```shell
 scp -r /public/home/lcq/rgzn/yuzhengmin/keti/database/sc_variant/table/scatac_susie/difference_gene root@bio.liclab.net:/mnt/data3/workspace/SCVdb/mysql/mysqlfile/scatac_susie/
 ```
+
+```shell
+scp -r /public/home/lcq/rgzn/yuzhengmin/keti/database/sc_variant/table/trs_big_susie  root@bio.liclab.net:/mnt/data3/workspace/SCVdb/data/data/download/
+```
+
--- R/exec_R_code/run.sh
+++ R/exec_R_code/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=scVMAP
-#SBATCH --mem-per-cpu=220gb
+#SBATCH --mem-per-cpu=3gb
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=64
 #SBATCH --output=scVMAP_%j.log
-#SBATCH --partition=debug
-
-# load the environment
-module load yzm_R4
+#SBATCH --partition=tyhcnormal
 
 # run R
-bash /public/home/ac1dyrvmyl/keti/scVMAP/code/exec_scavenge.sh /public/home/ac1dyrvmyl/keti/scVMAP 750c470706 655d030036_GSE139369_ELM_sim_0.65_ATAC.rds 84d769d9cd_BBJ_Mono_55.bed hg19 counts
+bash /work/home/ac1dyrvmyl/yzm/keti/scVMAP/code/exec_scavenge.sh /work/home/ac1dyrvmyl/yzm/keti/scVMAP 1ece220065 78f173d4eb_GSE139369_ELM_sim_0.8_ATAC.rds 113e64e245_BBJ_Mono_55.bed hg19 counts
