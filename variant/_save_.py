3be43fdcf69bc2e053efcf9d0c70dd5a0b1a0808
yuzhengmin <3236170161@qq.com>
2025-11-06 10:21:48 +0800

    feat: Add SCIV method

--- variant/_save_.py
+++ variant/_save_.py
@@ -5,26 +5,30 @@ import sciv
 if __name__ == '__main__':
 
     genomes: list = ["hg19", "hg38"]
+    methods: list = ["", "_susie"]
     database_path: str = "/public/home/lcq/rgzn/yuzhengmin/keti/database"
-    fine_mapping_result: dict = {}
 
-    for genome in genomes:
-        # variant files
-        variant_files = f"{database_path}/sc_variant/variant/{genome}"
+    for method in methods:
+        fine_mapping_result: dict = {}
 
-        # read variant information
-        variant_info = sciv.fl.read_variants(base_path=variant_files, column_map={0: "chr", 1: "position", 3: "rsId", 4: "pp"})
+        for genome in genomes:
+            print(f"genome: {genome}, method: {'FINEMAP' if method == '' else 'SuSiE'}")
+            # variant files
+            variant_files = f"{database_path}/sc_variant/variant{method}/{genome}"
 
-        # Add result
-        fine_mapping_result.update({genome: variant_info})
+            # read variant information
+            variant_info = sciv.fl.read_variants(base_path=variant_files, column_map={0: "chr", 1: "position", 3: "rsId", 4: "pp"})
+
+            # Add result
+            fine_mapping_result.update({genome: variant_info})
+
+            # Save variables to a file
+            sciv.fl.save_pkl(variant_info, f"{database_path}/sc_variant/variant{method}/fine_mapping_result_{genome}.pkl")
 
         # Save variables to a file
-        sciv.fl.save_pkl(variant_info, f"{database_path}/sc_variant/variant/fine_mapping_result_{genome}.pkl")
+        sciv.fl.save_pkl(fine_mapping_result, f"{database_path}/sc_variant/variant{method}/fine_mapping_result.pkl")
 
-    # Save variables to a file
-    sciv.fl.save_pkl(fine_mapping_result, f"{database_path}/sc_variant/variant/fine_mapping_result.pkl")
-
-    """python
-    # Recovering variables from files
-    fine_mapping_result = sciv.fl.read_pkl(f"{database_path}/sc_variant/variant/fine_mapping_result.pkl")
-    """
+        """python
+        # Recovering variables from files
+        fine_mapping_result = sciv.fl.read_pkl(f"{database_path}/sc_variant/variant{method}/fine_mapping_result.pkl")
+        """
